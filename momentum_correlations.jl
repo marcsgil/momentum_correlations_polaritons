d4d90470dda9874d18d3bd171dee0535a5688a78
Marcos Gil <marcosgildeoliveira@gmail.com>
2025-02-18 19:38:38 +0100

    draw more lines

--- momentum_correlations.jl
+++ momentum_correlations.jl
@@ -4,7 +4,7 @@ include("io.jl")
 include("polariton_funcs.jl")
 
 G2_r, G2_k, param, steady_state = h5open("correlation.h5", "r") do file
-    group = file["no_support_long"]
+    group = file["no_support"]
     read(group, "G2_r"), read(group, "G2_k"), read_parameters(group), read(group, "steady_state")
 end
 
