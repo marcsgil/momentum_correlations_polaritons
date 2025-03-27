153fd663efd45462158725413c8520d355f02e1c
Marcos Gil <marcosgildeoliveira@gmail.com>
2025-03-27 09:34:35 +0100

    tried new mean field configurations

--- TruncatedWigner/mean_field.jl
+++ TruncatedWigner/mean_field.jl
@@ -1,10 +1,10 @@
-using GeneralizedGrossPitaevskii, CUDA, CairoMakie
+using GeneralizedGrossPitaevskii, CairoMakie
 include("../polariton_funcs.jl")
 include("../io.jl")
 include("equations.jl")
 
 # Space parameters
-L = 1600.0f0
+L = 1600.0
 lengths = (L,)
 N = 1024
 δL = L / N
@@ -12,30 +12,30 @@ rs = range(; start=-L / 2, step=δL, length=N)
 ks = range(; start=-π / δL, step=2π / L, length=N)
 
 # Polariton parameters
-ħ = 0.6582f0 #meV.ps
-γ = 0.047f0 / ħ
-m = 1 / 6f0 # meV.ps^2/μm^2; This is 3×10^-5 the electron mass
+ħ = 0.6582 #meV.ps
+γ = 0.047 / ħ
+m = 1 / 6 # meV.ps^2/μm^2; This is 3×10^-5 the electron mass
 g = 3f-4 / ħ
 δ₀ = 0.49 / ħ
 
 # Potential parameters
-V_damp = 1000.0f0
-w_damp = 30.0f0
-V_def = 0.85f0 / ħ
-w_def = 5f0
+V_damp = 1000.0
+w_damp = 30.0
+V_def = 0.85 / ħ
+w_def = 5
 
 # Pump parameters
-k_up = 0.25f0
-k_down = 0.55f0
+k_up = 0.25
+k_down = 0.55
 divide = -7
 factor = 0.3
 
 ħ * k_up / m
 
 # Bistability cycle parameters
-Amax = 12f0
-t_cycle = 600.0f0
-t_freeze = 540.0f0
+Amax = 12
+t_cycle = 600.0
+t_freeze = 540.0
 
 dt = 2.0f-2
 nsaves = 512
@@ -44,9 +44,9 @@ nsaves = 512
 param = (; δ₀, m, γ, ħ, L, g, V_damp, w_damp, V_def, w_def,
     Amax, t_cycle, t_freeze, δL, N, k_down, k_up, divide, factor, dt)
 
-u0 = (zeros(ComplexF32, N),)
+u0 = (zeros(ComplexF64, N),)
 prob = GrossPitaevskiiProblem(u0, lengths; dispersion, potential, nonlinearity, pump, param)
-tspan = (0, 1200.0f0)
+tspan = (0, 1200.0)
 alg = StrangSplittingC()
 ts, sol = GeneralizedGrossPitaevskii.solve(prob, alg, tspan; dt, nsaves);
 steady_state = map(x -> x[:, end], sol)
